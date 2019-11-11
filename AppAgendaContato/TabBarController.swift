19c132b5ccef1f9a999ba797924e4be14b144c90
Willian de Lima <will_trk@hotmail.com>
2019-11-11 20:43:02 -0300

    Adicao viewControllers para algumas views e delecap de views que estavam a mais

---
+++ AppAgendaContato/TabBarController.swift
@@ -0,0 +1,13 @@
+//
+//  TabBarController.swift
+//  AppAgendaContato
+//
+//  Created by murillo castro on 11/11/19.
+//  Copyright © 2019 br.com.willtrkapp. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: TabBarController {
+
+}
